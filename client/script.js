ce6aa9663fe2516b62a76e5a1f0c44221f332c97
Stycket <121405173+Stycket@users.noreply.github.com>
2024-06-28 15:39:59 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -125,7 +125,7 @@ const handleSubmit = async (prompt) => {
 const sendToGPTAPI = async (prompt, messageDiv) => {
   const data = { prompt }
 
-  const response = await fetch('https://dragonpath.grabobastu.se', {
+  const response = await fetch('https://dragonpath.grabobastu.se/game', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
